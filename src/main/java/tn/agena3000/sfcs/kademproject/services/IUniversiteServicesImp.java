d9cbae6dd7b78f5589d2316e6c35b5d002ed6d08
IbrahimEsprit <ibrahim.barkallah@esprit.tn>
2023-02-24 22:37:28 +0100

    second push

--- src/main/java/tn/agena3000/sfcs/kademproject/services/IUniversiteServicesImp.java
+++ src/main/java/tn/agena3000/sfcs/kademproject/services/IUniversiteServicesImp.java
@@ -3,7 +3,9 @@ package tn.agena3000.sfcs.kademproject.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tn.agena3000.sfcs.kademproject.entities.Departement;
 import tn.agena3000.sfcs.kademproject.entities.Universite;
+import tn.agena3000.sfcs.kademproject.repositories.DepartementRepository;
 import tn.agena3000.sfcs.kademproject.repositories.UniversiteRepository;
 
 import java.util.List;
@@ -12,6 +14,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class IUniversiteServicesImp implements IUniversiteServices {
     private final UniversiteRepository universiteRepository;
+
+    private final DepartementRepository departementRepository;
 
     @Override
     public void ajoutUniversite(Universite u) {
@@ -36,7 +40,15 @@ public class IUniversiteServicesImp implements IUniversiteServices {
     @Override
     public void deleteUniversite(Integer id) {
         universiteRepository.deleteById(id);
+    }
+    @Override
+    public void assignUniversiteToDepartement(Integer idUniversite, Integer idDepartement) {
+        Universite universite = universiteRepository.findById(idUniversite).orElse(null);
+        Departement departement = departementRepository.findById(idDepartement).orElse(null);
 
-
+        if ((universite!=null) && (departement!=null)) {
+            universite.getDepartement().add(departement);
+            universiteRepository.save(universite);
+        }
     }
 }
